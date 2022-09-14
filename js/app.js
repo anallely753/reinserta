fa3f8f0ef407b9d673ae58d7205093f118bb7ebb
anallelyxdata <anallely@x-data.mx>
2022-09-14 04:02:21 -0500

    navbar mobile

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("wrapper").style.borderBottomLeftRadius = "25px";
+    document.getElementById("wrapper").style.borderBottomRightRadius = "25px";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+window.addEventListener("scroll", function(){
+  if(window.scrollY==0){
+    document.getElementById("wrapper").style.borderRadius = "0px";
+  }
+});
+
+const burgerMenu = document.getElementById("burger");
+const navbarMenu = document.getElementById("menu");
+
+// Show and Hide Navbar Menu
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("is-active");
+  navbarMenu.classList.toggle("is-active");
+
+  if (navbarMenu.classList.contains("is-active")) {
+    navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
+  } else {
+    navbarMenu.removeAttribute("style");
+  }
+});
